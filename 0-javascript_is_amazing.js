c885de205c8e30989b99371e76c995e78425be92
bekyman <51075542+bekyman@users.noreply.github.com>
2025-11-07 21:24:04 +0300

    Update 0-javascript_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,2 +1,7 @@
 let myVar = "JavaScript is amazing";
 console.log(myVar);
+
+let fun = "C is fun";
+let Python = "Python is cool";
+let JavaScript = "JavaScript is amazing";
+console.log(fun, Python, JavaScript);
