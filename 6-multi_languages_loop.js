ceb1282db23c7ca0ae4a5d6e177fe6fb6a6b1f54
bekyman <51075542+bekyman@users.noreply.github.com>
2025-11-08 19:37:42 +0300

    Correct loop implementation in multi_languages_loop.js
    Fix loop structure and syntax errors in multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,11 @@
 Const valu = [“C is fun”,“Python is cool”,“JavaScript is amazing”]; 
 
-for (i =0, i < valu.length, i ++){ 
+/*for (i =0, i < valu.length, i ++){ 
      console.log(valu[i]);
   i++;
 }
-
+*/
+while (i =0){
+  console.log(valu[i]);
+  i++;
+}
