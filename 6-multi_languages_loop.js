3632eb8531d0a7bdebad4f65ce5a87b583f71058
bekyman <51075542+bekyman@users.noreply.github.com>
2025-11-08 20:58:38 +0300

    Correct array syntax and implement while loop
    Fix syntax errors and update loop structure.

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,10 +1,6 @@
-Const valu = [“C is fun”,“Python is cool”,“JavaScript is amazing”]; 
+Const valu = ["C is fun","Python is cool","JavaScript is amazing"]; 
 
-/*for (i =0, i < valu.length, i ++){ 
-     console.log(valu[i]);
-  i++;
-}
-*/
+
 while (i =0){
   console.log(valu[i]);
   i++;
