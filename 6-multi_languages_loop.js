c35033099367f94cbfdf2ef4f30660ffca9dd30e
bekyman <51075542+bekyman@users.noreply.github.com>
2025-11-08 21:04:26 +0300

    Fix multi_languages_loop.js syntax and logic errors

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,7 @@
-Const valu = ["C is fun","Python is cool","JavaScript is amazing"]; 
-
-
-while (i =0){
+#!/usr/bin/node
+const valu = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0; 
+while (i < valu.length) { 
   console.log(valu[i]);
-  i++;
+  i++
 }
